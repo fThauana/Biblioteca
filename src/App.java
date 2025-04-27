5b2a7f5cf38a1e6831e170c7e0eec9f629ac9ee3
Kumegawa <lucaskumegawa222@gmail.com>
2025-04-27 01:39:44 -0300

    Atualizando o app

--- src/App.java
+++ src/App.java
@@ -1,6 +1,11 @@
 // TODO: nome e RGM e apagar comentarios (quando projeto estiver finalizado)
+// Lucas Kumegawa de Godoi RGM: 42045681
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Trabalho Java - Sistema de Gerenciamento de Biblioteca");
+        
+        view.BibliotecaView bibliotecaView = new view.BibliotecaView();
+        bibliotecaView.iniciar();
     }
 }
+
